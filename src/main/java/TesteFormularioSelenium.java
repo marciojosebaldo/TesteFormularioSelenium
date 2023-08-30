e2dea5bc3245034d9c282bde65dfb572c008f188
marciojosebaldo <marciojosebaldo@gmail.com>
2023-08-29 21:40:39 -0300

    Add files via upload

--- src/main/java/TesteFormularioSelenium.java
+++ src/main/java/TesteFormularioSelenium.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
@@ -18,10 +19,10 @@ public class TesteFormularioSelenium {
 
     @Test
     public void testarPaginaInicial() throws InterruptedException {
-        //Abrir a URL informada
+        // Abrir a URL informada
         driver.get("https://demo.automationtesting.in/");
 
-        //Incluir e-mail no campo
+        // Incluir e-mail no campo
         driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("marcio@teste.com");
 
         // Aqui faz aguardar 5 segundos para conferir como foi o teste
@@ -39,16 +40,40 @@ public class TesteFormularioSelenium {
         // Clica no botão enviar
         driver.findElement(By.xpath("//*[@id=\"enterimg\"]")).click();
 
-        //Preencher o campo First Name
+        // Preencher o campo First Name
         driver.findElement(By.xpath("//*[@id=\"basicBootstrapForm\"]/div[1]/div[1]/input")).sendKeys("marcio");
 
-        //Preencher o campo Last Name
+        // Preencher o campo Last Name
         driver.findElement(By.xpath("//*[@id=\"basicBootstrapForm\"]/div[1]/div[2]/input")).sendKeys("baldo");
 
-        /*
-                //Clicar no botão de Enter
-        driver.findElement(By.xpath("//*[@id=\"enterbtn\"]")).click();
+        // Preencher o campo Address
+        driver.findElement(By.xpath("//*[@id=\"basicBootstrapForm\"]/div[2]/div/textarea")).sendKeys("Rua Napoleão Correa Lobo, 200. Pato Branco-PR");
 
+        // Preencher o campo Email address*
+        driver.findElement(By.xpath("//*[@id=\"eid\"]/input")).sendKeys("marcio@marcio.com");
+
+        // Prencher o campo Phone*
+        driver.findElement(By.xpath("//*[@id=\"basicBootstrapForm\"]/div[4]/div/input")).sendKeys("4612345678");
+
+        // Preencher o campo Male na entrada tipo rádio
+        // Aqui também será utilizado o método clique
+        driver.findElement(By.xpath("//*[@id=\"basicBootstrapForm\"]/div[5]/div/label[1]")).click();
+
+        // Preencher dois campos no checkbox: segundo e terceiro. Lembre-se de escolher a tag input
+        driver.findElement(By.xpath("//*[@id=\"checkbox2\"]")).click();
+        driver.findElement(By.xpath("//*[@id=\"checkbox3\"]")).click();
+
+        // Preenche o campo Languages que é um tipo multi-select
+        // Para isso, foi criada uma variável do tipo WebElement e receber o XPath do elemento
+        WebElement multSelect = driver.findElement(By.xpath("//*[@id=\"basicBootstrapForm\"]/div[7]/div/multi-select"));
+        // Inclua o método click()
+        multSelect.click();
+        // Crie outra variável do tipo WebElement e receberá o texto Portugues
+        WebElement opcaoPortugues = driver.findElement(By.xpath("//a[contains(text(), 'Portuguese')]"));
+
+
+
+        /*
         //Armazena o texto de erro do elemento
         String texto = driver.findElement(By.xpath("//*[@id=\"errormsg\"]")).getText(); // Digitar aqui o xpath do elemento da mensagem
 
